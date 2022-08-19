a4ae356746814ea2ef05bb9530de9aa042dd1223
yangqs6 <yangqs@umich.edu>
2022-08-19 14:21:19 -0700

    Test Jenkins

---
+++ src/main/java/com/example/demo/Demo4Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Demo4Application.class, args);
+        System.out.println("Jenkins test");
+    }
+
+}
